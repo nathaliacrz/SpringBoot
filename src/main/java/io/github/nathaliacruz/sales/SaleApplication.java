bcab8259f1461a8d4d253e032e68fab2441876c3
Nathalia Cruz <nathalias161@gmail.com>
2024-01-31 04:30:30 -0300

    Component Scan annotation, only use when you want to scan another package

--- src/main/java/io/github/nathaliacruz/sales/SaleApplication.java
+++ src/main/java/io/github/nathaliacruz/sales/SaleApplication.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author nath0
  */
 @SpringBootApplication
+@ComponentScan(basePackages = {"io.github.nathaliacruz.sales"})
 @RestController
 public class SaleApplication {
 
